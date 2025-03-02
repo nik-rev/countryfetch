c81554da005b2539e09a959b3a0c32768afc09c2
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-02 21:32:06 +0000

    feat: add function to get country code from a country

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,75 +1,84 @@
+use heck::ToPascalCase;
 use std::fmt::Write as _;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
-use std::sync::LazyLock;
 
-/// Location of the current script file
-static MANIFEST_DIR: LazyLock<PathBuf> =
-    LazyLock::new(|| PathBuf::from(env!("CARGO_MANIFEST_DIR")));
+fn main() {
+    // Location of the current script file
+    let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
 
-/// Path where generated code lives.
-static GENERATED_DIR: LazyLock<PathBuf> = LazyLock::new(|| {
-    MANIFEST_DIR
+    // Path where generated code lives.
+    let generated_dir = manifest_dir
         .join("..")
         .join("countryfetch")
         .join("src")
-        .join("generated")
-});
+        .join("generated");
 
-/// Path where are the .svg files for each of the flags
-static SVG_SRC_DIR: LazyLock<PathBuf> =
-    LazyLock::new(|| MANIFEST_DIR.join("flag-svgs").join("4x3"));
+    // Path where the .svg files for each of the flags are located.
+    let svg_src_dir = manifest_dir.join("flag-svgs").join("4x3");
 
-/// Re-exports of generated code for ease of use.
-static MOD_RS: LazyLock<PathBuf> = LazyLock::new(|| GENERATED_DIR.join("mod.rs"));
+    // Re-exports of generated code for ease of use.
+    let mod_rs = generated_dir.join("mod.rs");
 
-/// country.rs: Contains implementations of all methods for the Country enum.
-static COUNTRY_RS: LazyLock<PathBuf> = LazyLock::new(|| GENERATED_DIR.join("country.rs"));
+    // country.rs: Contains implementations of all methods for the Country enum.
+    let country_rs = generated_dir.join("country.rs");
 
-/// flag.rs: Contains a single implementation of the `Country::flag` method.
-/// Impl is in a separate file due to the huge size of this file.
-static FLAG_RS: LazyLock<PathBuf> = LazyLock::new(|| GENERATED_DIR.join("flag.rs"));
+    // flag.rs: Contains a single implementation of the `Country::flag` method.
+    // Impl is in a separate file due to the huge size of this file.
+    let flag_rs = generated_dir.join("flag.rs");
 
-use heck::ToPascalCase;
-
-fn main() {
-    std::fs::create_dir_all(GENERATED_DIR.clone()).unwrap();
+    std::fs::create_dir_all(generated_dir).unwrap();
 
     let mut country_enum = String::from(
         "\
-pub enum Countries {
+#![cfg_attr(rustfmt, rustfmt_skip)]
+#[derive(Eq, PartialEq, Copy, Clone, Ord, PartialOrd)]
+pub enum Country {
 ",
     );
 
     let mut country_enum_impl = String::from(
-        "pub impl Countries {
+        "impl Country {
 ",
     );
 
     let mut country_enum_impl_fn_from_str = String::from(
-        "    pub fn from_str(s: &str) -> Result<Self, ()> {
+        "    pub fn from_str(s: &str) -> Option<Self> {
         match s {
+",
+    );
+
+    let mut country_enum_impl_const_countries = String::from(
+        "    pub const ALL_COUNTRIES: &[Self] = &[
 ",
     );
 
     let mut country_enum_impl_fn_from_country_code = String::from(
-        "    pub fn from_country_code(s: &str) -> Result<Self, ()> {
+        "    pub fn from_country_code(s: &str) -> Option<Self> {
         match s {
 ",
     );
 
-    // because this one is so large, it goes into a separate file
-    let mut flag_rs = String::from(
-        "use super::Countries;
-
-impl Countries {
-    pub fn from_str(&self) -> &'static str {
+    let mut country_enum_impl_fn_country_code = String::from(
+        "    pub fn country_code(&self) -> &'static str {
         match self {
 ",
     );
 
-    for svg_flag in (*SVG_SRC_DIR).read_dir().unwrap().map(|a| a.unwrap()) {
+    // because this one is so large, it goes into a separate file
+    let mut flag_rs_contents = String::from(
+        "#![cfg_attr(rustfmt, rustfmt_skip)]
+
+use super::Country;
+
+impl Country {
+    pub fn flag(&self) -> &'static str {
+        match self {
+",
+    );
+
+    for svg_flag in (*svg_src_dir).read_dir().unwrap().map(|a| a.unwrap()) {
         // let img = {
         //     let svg_path = svg_flag.path();
         //     let tree = {
@@ -121,9 +130,17 @@ impl Countries {
 
         let ascii_buf = "temp";
 
-        let flag_name = svg_flag.file_name();
+        let file_name = svg_flag
+            .file_name()
+            .into_string()
+            .expect("filename only consists of ASCII characters");
 
-        let flag_name_enum_member = flag_name.clone().into_string().unwrap().to_pascal_case();
+        let flag_parts: Vec<&str> = file_name.split('.').collect();
+
+        let country_name = flag_parts[0];
+        let country_code = flag_parts[1];
+
+        let flag_name_enum_member = country_name.to_pascal_case();
 
         // Add enum member
         writeln!(&mut country_enum, "    {},", flag_name_enum_member).unwrap();
@@ -131,67 +148,94 @@ impl Countries {
         // Add match for Countries::from_str
         writeln!(
             &mut country_enum_impl_fn_from_str,
-            "            \"{}\" => Ok(Countries::{}),",
-            flag_name.to_str().expect("Only contains ASCII characters"),
-            flag_name_enum_member
+            "            \"{}\" => Some(Country::{}),",
+            country_name, flag_name_enum_member
         )
         .unwrap();
 
         // Add flag for each country
         writeln!(
-            &mut flag_rs,
-            "            Countries::{} => {},",
+            &mut flag_rs_contents,
+            "            Country::{} => {},",
             flag_name_enum_member, ascii_buf
+        )
+        .unwrap();
+
+        // Add country code for each country
+        writeln!(
+            &mut country_enum_impl_fn_country_code,
+            "            Country::{} => \"{}\",",
+            flag_name_enum_member, country_code
+        )
+        .unwrap();
+
+        // Add country to array (for iteration)
+        writeln!(
+            &mut country_enum_impl_const_countries,
+            "        Country::{},",
+            flag_name_enum_member
         )
         .unwrap();
 
         // Add match for Countries::from_country_code
         writeln!(
             &mut country_enum_impl_fn_from_country_code,
-            "            \"gb\" => Err(()),"
+            "            \"{}\" => Some(Country::{}),",
+            country_code, flag_name_enum_member
         )
         .unwrap();
     }
 
     writeln!(
         &mut country_enum_impl_fn_from_str,
-        "            _ => Err(()),
+        "            _ => None,
         }}
     }}"
     )
     .unwrap();
     writeln!(
-        &mut flag_rs,
+        &mut flag_rs_contents,
         "        }}
     }}
 }}"
     )
     .unwrap();
     writeln!(
+        &mut country_enum_impl_fn_country_code,
+        "        }}
+    }}"
+    )
+    .unwrap();
+    writeln!(
         &mut country_enum_impl_fn_from_country_code,
-        "            _ => Err(()),
+        "            _ => None,
         }}
     }}"
     )
     .unwrap();
     writeln!(&mut country_enum, "}}").unwrap();
+    writeln!(&mut country_enum_impl_const_countries, "    ];\n").unwrap();
 
     write!(
         &mut country_enum_impl,
-        "{country_enum_impl_fn_from_str}{country_enum_impl_fn_from_country_code}"
+        "{country_enum_impl_const_countries}{country_enum_impl_fn_country_code}{country_enum_impl_fn_from_str}{country_enum_impl_fn_from_country_code}"
     )
     .unwrap();
 
     writeln!(&mut country_enum_impl, "}}").unwrap();
 
-    let country_rs = format!("{country_enum}\n{country_enum_impl}");
+    let country_rs_contents = format!("{country_enum}\n{country_enum_impl}");
 
-    let mut country_file = File::create(COUNTRY_RS.clone()).unwrap();
-    let mut flag_file = File::create(FLAG_RS.clone()).unwrap();
-    let mut mod_file = File::create(MOD_RS.clone()).unwrap();
+    let mut country_file = File::create(country_rs).unwrap();
+    let mut flag_file = File::create(flag_rs).unwrap();
+    let mut mod_file = File::create(mod_rs).unwrap();
 
-    country_file.write_all(country_rs.as_bytes()).unwrap();
-    flag_file.write_all(flag_rs.as_bytes()).unwrap();
+    country_file
+        .write_all(country_rs_contents.as_bytes())
+        .unwrap();
+
+    flag_file.write_all(flag_rs_contents.as_bytes()).unwrap();
+
     mod_file
         .write_all(
             b"mod country;
