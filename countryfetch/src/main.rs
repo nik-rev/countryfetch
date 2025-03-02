cd541c0bcc20b75bd5fdb1c45c6ec58e2dc9adf4
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-02 23:18:55 +0000

    feat: obtain country information about public IP

--- countryfetch/src/main.rs
+++ countryfetch/src/main.rs
@@ -1,9 +1,48 @@
+use std::net::IpAddr;
+
+use serde::{Deserialize, Serialize};
+
 mod generated;
 
-fn main() {
-    for country in generated::Country::ALL_COUNTRIES {
-        let flag = country.flag();
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct IpResponse {
+    pub status: Option<String>,
+    pub message: Option<String>,
+    pub continent: Option<String>,
+    pub continent_code: Option<String>,
+    pub country: Option<String>,
+    pub country_code: Option<String>,
+    pub region: Option<String>,
+    pub region_name: Option<String>,
+    pub city: Option<String>,
+    pub district: Option<String>,
+    pub zip: Option<String>,
+    pub lat: Option<f64>,
+    pub lon: Option<f64>,
+    pub timezone: Option<String>,
+    pub currency: Option<String>,
+    pub mobile: Option<bool>,
+    pub proxy: Option<bool>,
+    pub hosting: Option<bool>,
+    pub query: Option<String>,
+}
 
-        println!("{flag}");
-    }
+async fn get_ip_data() -> Result<IpResponse, Box<dyn std::error::Error>> {
+    let ip = public_ip::addr().await.unwrap();
+
+    let ip_data = reqwest::get(format!(
+        "http://ip-api.com/json/{ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
+    )).await?.json::<IpResponse>().await?;
+
+    Ok(ip_data)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let data = get_ip_data().await.unwrap();
+
+    dbg!(data);
+
+    Ok(())
 }
