2a3b3ff89efcf8d9ecf8e504d4a0fe85893b0ead
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-02 18:34:25 +0000

    feat: save all flags as ascii colors

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,14 +1,14 @@
-use resvg::usvg;
+use std::io::Write;
 
-fn main() {
+fn svg_path_to_img(svg_path: std::path::PathBuf) -> image::DynamicImage {
     let tree = {
-        let opt = usvg::Options {
-            resources_dir: Some(std::path::PathBuf::from("uk.svg")),
+        let opt = resvg::usvg::Options {
+            resources_dir: Some(svg_path.to_path_buf()),
             ..Default::default()
         };
-        let svg_data = std::fs::read("uk.svg").unwrap();
+        let svg_data = std::fs::read(svg_path).unwrap();
 
-        usvg::Tree::from_data(&svg_data, &opt).unwrap()
+        resvg::usvg::Tree::from_data(&svg_data, &opt).unwrap()
     };
 
     let pixmap_size = tree.size().to_int_size();
@@ -19,7 +19,7 @@ fn main() {
 
     resvg::render(&tree, tiny_skia::Transform::default(), &mut pixmap.as_mut());
 
-    let png = pixmap
+    pixmap
         .encode_png()
         .map(std::io::Cursor::new)
         .map_err(drop)
@@ -28,21 +28,38 @@ fn main() {
                 .decode()
                 .map_err(drop)
         })
-        .expect("Format is not valid PNG");
+        .expect("Format is not valid PNG")
+}
 
-    let mut buf: Vec<u8> = Vec::new();
+fn main() {
+    let svg_flags = std::fs::read_dir(std::path::PathBuf::from(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/flag-svgs/4x3"
+    )))
+    .unwrap();
 
-    rascii_art::render_image(
-        &png,
-        &mut buf,
-        &rascii_art::RenderOptions::new()
-            .width(40)
-            .height(17)
-            .colored(true),
-    )
-    .expect("Could not render SVG");
+    let destination = std::path::PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/../flags"));
 
-    let ascii = String::from_utf8(buf).unwrap();
+    std::fs::create_dir_all(&destination).unwrap();
 
-    println!("{ascii}");
+    for svg_flag in svg_flags.map(|a| a.unwrap()) {
+        let img = svg_path_to_img(svg_flag.path());
+
+        let mut ascii_buf: Vec<u8> = Vec::new();
+
+        rascii_art::render_image(
+            &img,
+            &mut ascii_buf,
+            &rascii_art::RenderOptions::new()
+                .width(40)
+                .height(17)
+                .colored(true),
+        )
+        .expect("Could not render SVG");
+
+        let ascii_flag_destination = destination.join(svg_flag.file_name());
+        let mut file = std::fs::File::create(ascii_flag_destination).unwrap();
+
+        file.write_all(&ascii_buf).unwrap();
+    }
 }
