8fc1026f6059edffaed8a218ab774b0dc50d7e17
Nikita Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-03-03 20:18:21 +0000

    feat: fetch information about country

--- countryfetch/src/main.rs
+++ countryfetch/src/main.rs
@@ -1,46 +1,124 @@
-use std::net::IpAddr;
+use serde_query::{DeserializeQuery, Query};
+use std::{collections::HashMap, net::IpAddr};
 
 use serde::{Deserialize, Serialize};
 
 mod generated;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+/// Information obtained from the user's IP
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct IpResponse {
-    pub status: Option<String>,
+pub struct Location {
+    pub status: String,
     pub message: Option<String>,
-    pub continent: Option<String>,
-    pub continent_code: Option<String>,
-    pub country: Option<String>,
-    pub country_code: Option<String>,
-    pub region: Option<String>,
-    pub region_name: Option<String>,
-    pub city: Option<String>,
-    pub district: Option<String>,
-    pub zip: Option<String>,
-    pub lat: Option<f64>,
-    pub lon: Option<f64>,
-    pub timezone: Option<String>,
-    pub currency: Option<String>,
-    pub mobile: Option<bool>,
-    pub proxy: Option<bool>,
-    pub hosting: Option<bool>,
-    pub query: Option<String>,
+    pub continent: String,
+    pub continent_code: String,
+    pub country: String,
+    pub country_code: String,
+    pub region: String,
+    pub region_name: String,
+    pub city: String,
+    pub district: String,
+    pub zip: String,
+    #[serde(rename = "lat")]
+    pub latitude: f64,
+    #[serde(rename = "lon")]
+    pub longitude: f64,
+    pub timezone: String,
+    pub currency: String,
+    pub mobile: bool,
+    pub proxy: bool,
+    pub hosting: bool,
+    pub query: String,
 }
 
-async fn get_ip_data() -> Result<IpResponse, Box<dyn std::error::Error>> {
+/// Information obtained for a specific country
+#[derive(Default, Debug, Clone, PartialEq, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Country {
+    #[serde(rename = "name")]
+    country_name: Name,
+    #[serde(rename = "tld")]
+    pub top_level_domain: Vec<String>,
+    #[serde(rename = "cca2")]
+    pub country_code: String,
+    pub currencies: HashMap<String, Currency>,
+    pub languages: HashMap<String, String>,
+    #[serde(rename = "borders")]
+    pub neighbours: Vec<String>,
+    #[serde(rename = "area")]
+    pub area_km: f64,
+    #[serde(rename = "flag")]
+    pub emoji: String,
+    pub population: u32,
+    pub continents: Vec<String>,
+    #[serde(rename = "flags")]
+    pub flag: Flag,
+    pub coat_of_arms: CoatOfArms,
+}
+
+impl Country {
+    pub fn country_name(&self) -> &str {
+        &self.country_name.common
+    }
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Name {
+    pub common: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Flag {
+    #[serde(rename = "png")]
+    pub url: String,
+    #[serde(rename = "alt")]
+    pub description: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct CoatOfArms {
+    pub png: String,
+    pub svg: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Currency {
+    pub name: String,
+    pub symbol: String,
+}
+
+async fn get_data() -> Result<(Location, Country), Box<dyn std::error::Error>> {
     let ip = public_ip::addr().await.unwrap();
+    let client = reqwest::Client::new();
 
-    let ip_data = reqwest::get(format!(
+    let ip_data = client.get(format!(
         "http://ip-api.com/json/{ip}?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,timezone,offset,currency,isp,org,as,asname,reverse,mobile,proxy,hosting,query"
-    )).await?.json::<IpResponse>().await?;
+    )).send().await?.json::<Location>().await?;
 
-    Ok(ip_data)
+    let country = client
+        .get(format!(
+            "https://restcountries.com/v3.1/alpha/{}",
+            ip_data.country_code
+        ))
+        .send()
+        .await?
+        .json::<Vec<Country>>()
+        .await?
+        .into_iter()
+        .next()
+        .expect("Returns a single country from querying for a Country Code, when there is a valid country code (which would have failed earlier)");
+
+    Ok((ip_data, country))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let data = get_ip_data().await.unwrap();
+    let data = get_data().await.unwrap();
 
     dbg!(data);
 
