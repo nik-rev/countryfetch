72254850e8b323f31fa957dfdeec68b74a691b14
Nik Revenco <154856872+NikitaRevenco@users.noreply.github.com>
2025-05-31 04:35:57 +0100

    refactor: rename module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 //! Countryfetch
 
-pub mod args;
 pub mod cache;
+pub mod cli;
 pub mod country;
 pub mod country_format;
 pub mod extra_country_data;
 pub mod generated_country_data;
 pub mod location;
 
-pub use args::Args;
+pub use cli::Args;
 pub use country::Country;
 pub use location::Location;
